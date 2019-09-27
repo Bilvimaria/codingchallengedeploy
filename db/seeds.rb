9f303e20afcb7152d93bceeb69a253a04d2979b3
bilvimaria <bilvimariajoseph@gmail.com>
2019-09-27 18:37:54 +0000

    Updates seed file for small testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+User.create(email: "bilsojoseph@gmail.com", password: "password")
+User.create(email: "biniljoseph@gmail.com", password: "password")
+User.create(email: "basiljoseph@gmail.com", password: "password")
+
+Article.destroy_all
+Article.create(title: 'articlethree’, body: 'Body3', user_id: 3)
+Article.create(title: 'articlefour’, body: 'Body4', user_id: 3)
+Article.create(title: 'articlefive’, body: 'Body5', user_id: 3)
+Article.create(title: 'articlethree’, body: 'Body3', user_id: 4)
+Article.create(title: 'articlefour’, body: 'Body4', user_id: 4)
+Article.create(title: 'articlefive’, body: 'Body5', user_id: 4)
